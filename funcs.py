35858daca47087b3b8d1f41060255eca6646fd47
HAN <han82@mail.ru>
2023-09-11 01:43:10 +0300

    funcs.py

---
+++ funcs.py
@@ -0,0 +1,8 @@
+
+def calculator(sum):
+    """Функция складывает два числа"""
+    print('Введите числа:')
+    number_1 = int(input())
+    number_2 = int(input())
+    sum(number_1 + number_2)
+    return sum
