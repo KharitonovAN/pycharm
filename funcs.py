5df2954b25465cb704c04aec4ab08e56cf4988a7
HAN <han82@mail.ru>
2023-09-11 01:50:49 +0300

    funcs.py

--- funcs.py
+++ funcs.py
@@ -4,5 +4,5 @@ def calculator(sum):
     print('Введите числа:')
     number_1 = int(input())
     number_2 = int(input())
-    sum(number_1 + number_2)
+    sum = number_1 + number_2
     return sum
